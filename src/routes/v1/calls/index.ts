e459b08166fa816c5830b7c3ca4583fd9dcdea74
SIARHEI-SMANTSAR <siarhei.smantsar@gmail.com>
2022-10-28 01:27:42 +0200

    feat: add POST: /api/v1/calls

--- src/routes/v1/calls/index.ts
+++ src/routes/v1/calls/index.ts
@@ -1,5 +1,6 @@
 import express, { Request } from 'express';
-import { compose, propOr, values } from 'ramda';
+import { compose, propOr, values, groupBy, toPairs } from 'ramda';
+import { saveCalls } from '@unitless-io/local-db';
 
 import { callsByFunctionMap, getFunctionId } from '@app/db';
 import queryValidationFactory from '@app/middlewares/query-validation';
@@ -11,23 +12,34 @@ interface CallsRouteQuery {
   funcName: string;
 }
 
-callsRouter.use(queryValidationFactory<CallsRouteQuery>(['fileId', 'funcName']));
+callsRouter.get(
+  '/',
+  queryValidationFactory<CallsRouteQuery>(['fileId', 'funcName']),
+  async (req: Request<{}, any, any, CallsRouteQuery>, res) => {
+    try {
+      const functionId = getFunctionId(req.query);
 
-callsRouter.get('/', async (req: Request<{}, any, any, CallsRouteQuery>, res) => {
-  try {
-    const functionId = getFunctionId(req.query);
+      // eslint-disable-next-line
+      const calls = compose(values, propOr({}, functionId))(callsByFunctionMap);
 
-    // eslint-disable-next-line
-    const calls = compose(
-      values,
-      propOr({}, functionId),
-    )(callsByFunctionMap);
-
-    res.status(200).send(calls);
-  } catch (error: any) {
-    console.error(error);
-    res.status(500).send(error?.message);
+      res.status(200).send(calls);
+    } catch (error: any) {
+      console.error(error);
+      res.status(500).send(error?.message);
+    }
   }
+);
+
+type PostReqBody = { id: string; args: string; result: string }[];
+// POST: /api/v1/calls
+callsRouter.post('/', async (req, res) => {
+  res.status(200).send();
+
+  const data: PostReqBody = req.body || [];
+
+  toPairs(groupBy(({ id }) => id, data)).forEach(async ([id, calls]) => {
+    saveCalls(id, calls);
+  });
 });
 
 export default callsRouter;

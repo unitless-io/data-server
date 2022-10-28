ef28a4615da7137d5cd0243b23623b0a86404124
SIARHEI-SMANTSAR <siarhei.smantsar@gmail.com>
2022-10-28 21:37:40 +0200

    chore: update @unitless-io/local-db

--- src/helpers/get-test-file-name/index.ts
+++ src/helpers/get-test-file-name/index.ts
@@ -1,7 +1,4 @@
-import compose from 'ramda/src/compose';
-import split from 'ramda/src/split';
-import replace from 'ramda/src/replace';
-import last from 'ramda/src/last';
+import { compose, split, replace, last } from 'ramda';
 
 export const getTestFileName = (filePath: string, functionName: string) =>
   compose(replace(/\.([jt]sx?)$/g, `-${functionName}.test.$1`), last, split('/'))(filePath);
